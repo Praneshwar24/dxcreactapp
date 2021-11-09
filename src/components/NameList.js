54d0eefb34e4b77c8776a1a022b2e14f37c20f67
Praneshwar <praneshwar.prakasam24@gmail.com>
2021-11-09 18:00:47 +0530

    Person.js seperated person component from person list component

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Person from './Person'
 
 export default function NameList() {
 
@@ -26,7 +27,8 @@ export default function NameList() {
             skill: 'FullStack'
         },
     ]
-    const personList = persons.map(person => <h2>I am {person.name}, I am {person.age} years old and my skill is {person.skill}</h2>)
+    /* const personList = persons.map(person => <h2>I am {person.name}, I am {person.age} years old and my skill is {person.skill}</h2>) */
+    const personList = persons.map(person => (<Person human={person}></Person>))
     return (
         <div>{personList}</div>
     )
