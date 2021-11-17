6805c3980a498c79c2ed4f9ddc3babff52b88f6b
Praneshwar24 <praneshwar.prakasam24@gmail.com>
2021-11-17 16:31:04 +0530

    useState with object and spread operator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import FrParentInput from './components/refs/FrParentInput';
-import PortalDemo from './components/portals/PortalDemo';
-import HookCounter from './components/hooks/HookCounter';
-import ClassCounter from './components/hooks/ClassCounter';
+
 import HookCounter2 from './components/hooks/HookCounter2';
+import HookCounter3 from './components/hooks/HookCounter3';
 
 
 
@@ -12,10 +10,7 @@ import HookCounter2 from './components/hooks/HookCounter2';
 function App() {
   return (
     <div className="App">
-      {/* <FrParentInput /> */}
-     {/*  <PortalDemo />
-      <HookCounter /> */}
-      <HookCounter2/>
+      <HookCounter3 />
     </div>
   );
 }
