8aef716357a29cef3cea3cd4268126da6c18de86
Praneshwar24 <praneshwar.prakasam24@gmail.com>
2021-11-17 15:56:38 +0530

    Previous state using hooks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FrParentInput from './components/refs/FrParentInput';
 import PortalDemo from './components/portals/PortalDemo';
 import HookCounter from './components/hooks/HookCounter';
 import ClassCounter from './components/hooks/ClassCounter';
+import HookCounter2 from './components/hooks/HookCounter2';
 
 
 
@@ -12,8 +13,9 @@ function App() {
   return (
     <div className="App">
       {/* <FrParentInput /> */}
-      <PortalDemo />
-      <HookCounter />
+     {/*  <PortalDemo />
+      <HookCounter /> */}
+      <HookCounter2/>
     </div>
   );
 }
