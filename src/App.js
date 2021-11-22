a60ccc47cb96072b7b884807eeaee5876418df1a
Praneshwar24 <praneshwar.prakasam24@gmail.com>
2021-11-22 10:54:54 +0530

    counter useReducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import ComponentA from './components/contexthook/ComponentA';
+
 import React from 'react';
-export const UserContext = React.createContext()   //exporting a context
-export const BatchContext = React.createContext()
+import CounterOne from './components/reducerhook/CounterOne';
+
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'Praneshwar'}>
-        <BatchContext.Provider value={'DXC Batch 2'}>
-          <ComponentA />
-        </BatchContext.Provider>
-      </UserContext.Provider>
+      <CounterOne />
     </div>
   );
 }
